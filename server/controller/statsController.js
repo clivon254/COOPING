15f8bd4fced3d591cd8c3a2570e70d0a18cf7381
Clivon Osire <clivon254@icloud.com>
2025-01-23 06:27:31 +0300

    Way maker

--- server/controller/statsController.js
+++ server/controller/statsController.js
@@ -1,1 +1,68 @@
+import Product from "../model/productModel"
+import User from "../model/userModel"
+import { errorHandler } from "../Utils/error"
 
+
+export const Adminstats = async (req,res,next) => {
+    
+    if(!req.user.isAdmin)
+    {
+        return next(errorHandler(403,"You are not allowed to access the access the stats"))
+    }
+
+    try
+    {
+        const {query} = req.query
+
+        const numofDays = Number(query) || 30
+
+        const startDate = new Date()
+
+        startDate.setDate(current.getDate() - numofDays)
+
+
+        const totalFood = await Product.find({type:Food}).countDocuments()
+
+
+        const totalMerchendise = await Product.find({type:Merchendise}).countDocuments()
+
+
+        const totalDrinks = await Product.find({type:Drinks}).countDocuments()
+
+
+        const totalLiquor = await Product.find({type:Liquor}).countDocuments()
+
+        const Users = await User.find().countDocuments()
+
+        
+    }
+    catch(error)
+    {
+        next(error)
+    }
+
+}
+
+
+export const stats = async (req,res,next) => {
+
+    try
+    {}
+    catch(error)
+    {
+        next(error)
+    }
+
+}
+
+
+export const Adminstat = async (req,res,next) => {
+
+    try
+    {}
+    catch(error)
+    {
+        next(error)
+    }
+
+}
