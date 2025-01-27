2601edf8cc752b29a9cec71974801711e51705fc
Clivon Osire <clivon254@icloud.com>
2025-01-27 08:39:41 +0300

    frontEnd-installtion

---
+++ admin/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+export default function App() {
+
+  return (
+
+    <div className="">
+       
+      <p className="text-3xl text-red-500 underline-offset-4">HELLO COOPING ADMIN</p>
+
+    </div>
+
+  )
+}
---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export default function App() {
+
+  return (
+
+    <div>
+
+      <p className="text-3xl text-green-700">HELLO COOPING CLIENT</p>
+
+    </div>
+
+  )
+
+}
