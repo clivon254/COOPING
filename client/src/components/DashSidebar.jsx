e4703c27c0e88b70fbd969eca62ce174213b89f3
Clivon Osire <clivon254@icloud.com>
2025-02-27 19:00:41 +0300

    navbar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,6 +3,9 @@
 import React, { useContext } from 'react'
 import { StoreContext } from '../context/store'
 import { NavLink } from 'react-router-dom'
+import { MdPermDeviceInformation } from 'react-icons/md'
+import { FaInfo , FaQuestion } from "react-icons/fa";
+
 
 export default function DashSidebar() {
 
@@ -12,14 +15,14 @@ export default function DashSidebar() {
 
     <>
 
-        <div className="flex flex-col gap-y-3">
+        <div className="flex flex-col gap-y-4">
 
             {NavLinks?.map((nav,index) => (
 
                     <NavLink
                         key={index}
                         to={`${nav.path}`}
-                        className={({isActive}) => isActive ? "flex items-center gap-x-1 text-sm font-semibold text-orange-600" : "flex items-center gap-x-1 text-sm font-semibold"}
+                        className={({isActive}) => isActive ? "flex items-center gap-x-1 text-base font-semibold text-primary" : "flex items-center gap-x-1 text-base font-semibold"}
                         onClick={() => setOpen(false)}
                     >
 
@@ -28,6 +31,36 @@ export default function DashSidebar() {
                     </NavLink>
 
             ))}
+
+            <NavLink
+                to={`/about`}
+                className={({isActive}) => isActive ? "flex items-center gap-x-1 text-base font-semibold text-primary" : "flex items-center gap-x-1 text-base font-semibold"}
+                onClick={() => setOpen(false)}
+            >
+
+            <span className=""><FaInfo/></span> About
+
+            </NavLink>
+
+            <NavLink
+                to={`/contact`}
+                className={({isActive}) => isActive ? "flex items-center gap-x-1 text-base font-semibold text-primary" : "flex items-center gap-x-1 text-base font-semibold"}
+                onClick={() => setOpen(false)}
+            >
+
+              <span className=""><MdPermDeviceInformation/></span> contact
+
+            </NavLink>
+
+            <NavLink
+                to={`/faq`}
+                className={({isActive}) => isActive ? "flex items-center gap-x-1 text-base font-semibold text-primary" : "flex items-center gap-x-1 text-base font-semibold"}
+                onClick={() => setOpen(false)}
+            >
+
+              <span className=""><FaQuestion/></span> FAQ
+
+            </NavLink>
 
         </div>
     
